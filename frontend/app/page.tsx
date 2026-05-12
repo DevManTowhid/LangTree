0dfe99d205eebe88ba94db86b91c3ed2806e109d
DevManTowhid <mdtowhidulalam364@gmail.com>
2026-05-12 22:28:34 +0600

    added language families as js array

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,31 @@
 import Image from "next/image";
 
+
+const languageFamilies = [
+  "Afroasiatic", "Algic", "Amto-Musan", "Arauan", "Araucanian", "Arawakan", 
+  "Arutani-Sape", "Australian (Pama-Nyungan)", "Austroasiatic", "Austronesian", 
+  "Aymaran", "Barbacoan", "Bayono-Awbono", "Boran", "Caddoan", "Cahuapanan", 
+  "Cariban", "Chapacuran", "Chibchan", "Chimakuan", "Chocoan", "Chon", 
+  "Chukotko-Kamchatkan", "Chumashan", "Cochimi-Yuman", "Dravidian", 
+  "East Bird's Head-Sentani", "Eskimo-Aleut", "Guaicuruan", "Harakmbut", 
+  "Hmong-Mien", "Indo-European", "Iroquoian", "Japonic", "Jivaroan", 
+  "Kartvelian (South Caucasian)", "Katukinan", "Keresan", "Khoe-Kwadi", 
+  "Kiowa-Tanoan", "Koreanic", "Kra-Dai", "Kx'a", "Left May", "Lower Mamberamo", 
+  "Macro-Ge", "Maku", "Mande", "Mascoian", "Matacoan", "Mayan", "Misumalpan", 
+  "Mixe-Zoquean", "Mongolic", "Mura-Pirahã", "Muskogean", "Nambikwaran", 
+  "Niger-Congo", "Nilo-Saharan", "Nimboran", "North Caucasian", "Nyulnyulan", 
+  "Oto-Manguean", "Pano-Tacanan", "Pauwasi", "Penutian", "Pomoan", "Quechuan", 
+  "Ramu-Lower Sepik", "Salishan", "Sepik", "Sino-Tibetan", "Siouan-Catawban", 
+  "Skou", "South Bougainville", "Tai-Kadai", "Tequistlatecan", "Tor-Kwerba", 
+  "Torricelli", "Totonacan", "Trans-New Guinea", "Tucanoan", "Tungusic", 
+  "Tupian", "Turkic", "Tuu", "Uralic", "Uto-Aztecan", "Wakashan", "West New Britain", 
+  "Wintuan", "Witotoan", "Yanomaman", "Yeniseian", "Yok-Utian", "Yukaghir", 
+  "Yuki-Wappo", "Zamucoan", "Zaparoan"
+];
+
+
+
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
